eed8227c9a69d8448810f31f64475b8f346ca85e
PlayerG9 <g9yout@gmail.com>
2021-09-27 17:15:59 +0200

    added coding and basic functions

--- src/mathcompiler/mathcompiler.py
+++ src/mathcompiler/mathcompiler.py
@@ -1,3 +1,16 @@
+# -*- coding=utf-8 -*-
+import math
+
+
+default_functions = {
+    'sin': math.sin,
+    'cos': math.cos,
+    'tan': math.tan,
+    'pow': math.pow,
+    'sqrt': math.sqrt,
+}
+
+
 class MathCompiler:
     def compute(self, equation: str, variables: dict) -> float:
         pass
