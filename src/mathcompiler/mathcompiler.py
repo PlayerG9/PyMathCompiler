c1da6b4c263e39a2e4b2c2daf6bde5e22325c4e2
PlayerG9 <g9yout@gmail.com>
2021-09-28 11:57:00 +0200

    created basic structure

--- src/mathcompiler/mathcompiler.py
+++ src/mathcompiler/mathcompiler.py
@@ -1,6 +1,11 @@
 # -*- coding=utf-8 -*-
+r"""
+# noinspection PyTypeChecker
+"""
 import math
 import re
+import functools
+import typing as t
 import logging
 from .exceptions import *
 
@@ -22,19 +27,38 @@ default_constants = {
     'inf': math.inf
 }
 
-default_dict = {**default_functions, **default_constants}  # combine both
+# default_dict = {**default_functions, **default_constants}  # combine both
 
 
 class MathCompiler:
-    def compute(self, equation: str, variables: dict) -> float:
+    solve_braces = re.compile(r'')
+    solve_dots = re.compile(r'')
+    solve_lines = re.compile(r'')
+    solve_statement = re.compile(r'')
+
+    def __init__(self, functions: dict, constants: dict):
+        self.functions = functions
+        self.constants = constants
+
+    def compute(self, equation: str, variables: dict, auto_round: bool = True) -> float:
+        formula = self.compile(equation)
+        result = self.solve(formula, variables)
+
+        if auto_round and result % 1 == 0:
+            return int(result)
+        else:
+            return result
+
+    def compile(self, equation: str) -> t.List:
+        pass
+
+    def solve(self, formula: list, variables: dict) -> float:
         pass
 
 
-default_compiler = MathCompiler()
+default_compiler = MathCompiler(default_functions, default_constants)
 
-compute = default_compiler.compute
 
-# import functools
-# @functools.wraps(default_compiler.compute)
-# def compute(equation: str, **kwargs) -> float:
-#     return default_compiler.compute(equation, kwargs)
+@functools.wraps(default_compiler.compute)
+def compute(equation: str, **kwargs) -> float:
+    return default_compiler.compute(equation, kwargs)
