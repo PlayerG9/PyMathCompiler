43858612be78c9640399c7de0fb56ab201f2c737
PlayerG9 <g9yout@gmail.com>
2021-10-10 20:41:06 +0200

    added new exception: MissingBracketError

--- src/mathcompiler/exceptions.py
+++ src/mathcompiler/exceptions.py
@@ -2,3 +2,4 @@ class MathError(Exception): pass
 class NumberParseError(MathError): pass
 class VariableMissingError(MathError): pass
 class FunctionMissingError(MathError): pass
+class MissingBracketError(MathError): pass
